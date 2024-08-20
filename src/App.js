125eb8820da0150dd7c0a736d0b7ec0c9b9547b7
ipscoder <psawant0015@gmail.com>
2024-08-21 00:04:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// Counter.js
+import React from 'react';
+import useStore from './store';
 
 function App() {
+  
+  const { count, increase, decrease, reset } = useStore();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
